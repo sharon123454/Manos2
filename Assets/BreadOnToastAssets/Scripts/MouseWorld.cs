bd74aabb4fc9a3ec24ed650e9515e006305caa05
DSlayer <93089045+sharon123454@users.noreply.github.com>
2024-04-03 23:25:24 +0300

    Mouse raycast + basic movement

---
+++ Assets/BreadOnToastAssets/Scripts/MouseWorld.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class MouseWorld : MonoBehaviour
+{
+    private static MouseWorld instance;
+
+    private string[] _activeLayerNames = { "MousePlane"/*, "Unit",*/ };
+    private LayerMask _activeMouseLayerMask;
+    private bool _isActive = true;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+            Destroy(gameObject);
+
+        instance = this;
+    }
+    private void Start()
+    {
+        _activeMouseLayerMask.value = LayerMask.GetMask(_activeLayerNames);
+    }
+    
+    public static Vector3 GetPosition()
+    {
+        if (instance._isActive)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray, out RaycastHit rayCastHit, float.MaxValue, instance._activeMouseLayerMask);
+            return rayCastHit.point;
+        }
+        return Vector3.zero;
+    }
+
+    private void ToggleMouseWorld() { _isActive = !_isActive; }
+
+}
---
+++ Assets/BreadOnToastAssets/Scripts/Unit.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    private Vector3 _targetPosition;
+    private float _stoppingDistance = 0.1f;
+    private float _moveSpeed = 4f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            _targetPosition = MouseWorld.GetPosition();
+        }
+
+        if (Vector3.Distance(transform.position, _targetPosition) > _stoppingDistance)//Stops jittering from never reaching clean position
+        {
+            Vector3 moveDir = (_targetPosition - transform.position).normalized;
+            transform.position += moveDir * _moveSpeed * Time.deltaTime;
+        }
+    }
+
+    private void Move(Vector3 targetPosition)
+    {
+        _targetPosition = targetPosition;
+    }
+
+}
