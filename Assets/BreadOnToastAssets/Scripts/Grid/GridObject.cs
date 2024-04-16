5420752238b387d8e58c478fd763086719866b97
DSlayer <93089045+sharon123454@users.noreply.github.com>
2024-04-16 19:22:11 +0300

    Grid base

---
+++ Assets/BreadOnToastAssets/Scripts/Grid/GridObject.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class GridObject : MonoBehaviour
+{
+    private GridSystem _gridSystem;
+    private GridPosition _gridPosition;
+
+    public GridObject(GridSystem gridSystem, GridPosition gridPosition)
+    {
+        _gridSystem = gridSystem;
+        _gridPosition = gridPosition;
+    }
+
+}
---
+++ Assets/BreadOnToastAssets/Scripts/Grid/GridSystem.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public struct GridPosition
+{
+    public int _x;
+    public int _z;
+
+    public GridPosition(int x, int z) { _x = x; _z = z; }
+    public override string ToString() { return $"X: {_x}; Z: {_z}"; }
+}
+
+public class GridSystem : MonoBehaviour
+{
+    private int _width;
+    private int _height;
+    private float _cellSize;
+
+    public GridSystem(int width, int height, float cellSize)
+    {
+        _width = width;
+        _height = height;
+        _cellSize = cellSize;
+
+        for (int x = 0; x < _width; x++)
+        {
+            for (int z = 0; z < _height; z++)
+            {
+                Debug.DrawLine(GetWorldPosition(x,z), GetWorldPosition(x,z) + Vector3.right * 0.2f, Color.white, 1000);
+            }
+        }
+
+    }
+
+    public Vector3 GetWorldPosition(int x, int z)
+    {
+        return new Vector3(x, 0, z) * _cellSize;
+    }
+
+    public GridPosition GetGridPosition(Vector3 worldPosition)
+    {
+        return new GridPosition(
+            Mathf.RoundToInt(worldPosition.x / _cellSize),
+            Mathf.RoundToInt(worldPosition.z / _cellSize));
+    }
+
+}
